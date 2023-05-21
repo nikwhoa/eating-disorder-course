102b16dec52e812287973f0cfb88e727e6ab320a
Nikita <norenkonikita@gmail.com>
2023-05-21 21:42:32 +0300

    qqq

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
-const cors = require('@fastify/cors')
+const fs = require('fs');
+const path = require('path');
+const cors = require('@fastify/cors');
 const fastify = require('fastify')({
-  logger: true
-})
+  logger: true,
+  https: {
+    key: fs.readFileSync(path.join('/etc/letsencrypt/live/eating-disorder-course.norenko.net.ua/privkey.pem')),
+    cert: fs.readFileSync(path.join('/etc/letsencrypt/live/eating-disorder-course.norenko.net.ua/fullchain.pem')),
+  },
+  http2: true,
+});
 
 fastify.register(cors, {
   origin: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
-})
+});
 
 fastify.get('/api/questions', async (request, reply) => {
   // send json data from file to client
@@ -15,8 +22,7 @@ fastify.get('/api/questions', async (request, reply) => {
     .code(200)
     .header('Content-Type', 'application/json; charset=utf-8')
     .send(require('./app/questions/questions.json'))
-})
-
+});
 
 fastify.listen({ port: 4000, host: '0.0.0.0' }, function (err, address) {
   if (err) {
@@ -24,4 +30,4 @@ fastify.listen({ port: 4000, host: '0.0.0.0' }, function (err, address) {
     process.exit(1)
   }
   fastify.log.info(`server listening on ${address}`)
-})
+});
