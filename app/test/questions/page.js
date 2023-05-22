abbc50cbbdc44523567d23f83fad65b2f560c40d
Nikita <norenkonikita@gmail.com>
2023-05-22 11:05:31 +0300

    optimizing questions page

--- app/test/questions/page.js
+++ app/test/questions/page.js
@@ -1,29 +1,16 @@
 'use client';
-
-import React, { useEffect, useState } from 'react';
-import useSWR from 'swr';
+import questions from './questions';
 
 export default function Page() {
-  const [dataQuestions, setDataQuestions] = useState([]);
-
-  const fetcher = (url) => fetch(url).then((res) => res.json());
-  const { data, error, isLoading } = useSWR(
-    `${process.env.DB_HOST}:4000/api/questions`,
-    fetcher
-  );
-
-
-  if (error) return "An error has occurred.";
-  if (isLoading) return "Loading...";
   return (
     <>
       <div className="lg:max-w-lg">
         <ul className="mt-6">
-          {/* {data.map((question, i) => (
+          {questions.map((question, i) => (
             <li className="mt-4 text-xl" key={question.id}>
               {i + 1}. {question.question}
             </li>
-          ))} */}
+          ))}
         </ul>
       </div>
     </>
