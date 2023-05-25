769aa38f5df247e404e27e2d1deadba88a29ee3e
Nikita <norenkonikita@gmail.com>
2023-05-25 19:50:41 +0300

    test styles changes

--- app/page.js
+++ app/page.js
@@ -5,14 +5,13 @@ export default function Home() {
     // <main className="flex h-screen flex-col items-center justify-between p-24">
     <main className="flex flex-col items-center justify-between">
       <div className="lg:max-w-lg test-container">
-        <div className="p-5">
+        <div className="p-5 pl-2 pr-2">
           <h1 className="">Тест ставлення до прийому їжі</h1>
         </div>
         <div className="p-7 pt-1">
           <div className="test-info">
             <p className="text-base leading-7 text-gray-700 md:text-lg md:leading-9 test-welcome-text">
-              Розлади харчової поведінки - це не просто проблеми зі столовою ложкою чи вагами. Це серйозні, часто приховані, психологічні та фізіологічні лиха, що подібні до крижаних айсбергів в океані життя. Немов невидимі ланцюги, вони утримують людей у полоні страху і самовіддачі, отруюючи їхнє
-              ставлення до власного тіла і свого здоров&apos;я. Руйнування цих ланцюгів важливе, щоб повернути людям їхню внутрішню свободу і дозволити їм повністю насолоджуватися життям.
+            Тест складається із 26 питань та дозволяє визначити наявність розладу харчової поведінки
             </p>
           </div>
         </div>
