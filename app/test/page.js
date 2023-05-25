81aec21189dcdefa59f365bc403d81d827356e48
Nikita <norenkonikita@gmail.com>
2023-05-25 22:24:29 +0300

    results changes

--- app/test/page.js
+++ app/test/page.js
@@ -3,7 +3,7 @@ import Question from "./Question";
 export default function Page() {
   return (
     <>
-      <div className="lg:max-w-lg">
+      <div className="lg:max-w-lg pb-12">
         <Question />
       </div>
     </>
