107890c0b93b4d0495f83d13c713957ca8a9950c
Nikita <norenkonikita@gmail.com>
2023-06-02 16:18:07 +0300

    site title change

--- app/layout.js
+++ app/layout.js
@@ -1,29 +1,29 @@
-import './styles/globals.scss'
+import './styles/globals.scss';
 
 export const metadata = {
-  title: 'Розбираємося з РПП: Онлайн Курс',
-  description: "Розберіться в проблемі РХП, дізнайтеся про стратегії подолання і почніть свій шлях до здоров'я.",
-  favicon: './favicons/favicon.ico',
-}
+  title: 'Ставлення до прийому їжі — Eating Attitudes Test (EAT-26)',
+  description: 'Тест ставлення до прийому їжі (Eating Attitudes Test (EAT-26))',
+  favicon: './favicons/favicon.ico'
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#707070" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </head>
-      <body className={'no-transition'}>
-        {children}
-      </body>
+    <html lang='en'>
+    <head>
+      <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+      <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+      <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+      <link rel='manifest' href='/site.webmanifest' />
+      <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#707070' />
+      <meta name='msapplication-TileColor' content='#da532c' />
+      <meta name='theme-color' content='#ffffff' />
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+    </head>
+    <body className={'no-transition'}>
+    {children}
+    </body>
 
     </html>
 
-  )
+  );
 }
