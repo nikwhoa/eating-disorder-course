93eaa01b39e1d90ab2f9557c68a44a5c27fc76a6
Nikita <norenkonikita@gmail.com>
2023-06-08 03:24:01 +0300

    main sections styles changes

--- app/kursrhp/page.js
+++ app/kursrhp/page.js
@@ -1,10 +1,12 @@
 import "@/app/kursrhp/styles.scss";
 import Main from '@/app/kursrhp/components/main/main';
+import CourseFor from '@/app/kursrhp/components/course-for/course-for';
 
 export default function KursRHPPage() {
   return (
     <>
       <Main />
+      <CourseFor />
     </>
   );
 }
