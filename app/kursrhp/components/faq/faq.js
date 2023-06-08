67637a351b9cd323bf4960d9cda261afba0aea5e
Nikita <norenkonikita@gmail.com>
2023-06-08 23:28:24 +0300

    fix

--- app/kursrhp/components/faq/faq.js
+++ app/kursrhp/components/faq/faq.js
@@ -1,5 +1,5 @@
 'use client';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './faq.scss';
 import { CloseIcon, OpenIcon } from '@/app/tools/icons/icons';
 
