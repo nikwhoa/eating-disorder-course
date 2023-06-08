42030a5ad32badd003c9a63ea1cf6f4776f95aaf
Nikita <norenkonikita@gmail.com>
2023-06-08 04:05:37 +0300

    course format

--- app/kursrhp/page.js
+++ app/kursrhp/page.js
@@ -1,12 +1,14 @@
 import "@/app/kursrhp/styles.scss";
 import Main from '@/app/kursrhp/components/main/main';
 import CourseFor from '@/app/kursrhp/components/course-for/course-for';
+import CourseFormat from '@/app/kursrhp/components/course-format/course-format';
 
 export default function KursRHPPage() {
   return (
     <>
       <Main />
       <CourseFor />
+      <CourseFormat />
     </>
   );
 }
