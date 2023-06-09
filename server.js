a3a97a53c2c0dee11e2c4247bb1d29751c7e3876
Nikita <norenkonikita@gmail.com>
2023-06-09 22:11:24 +0300

    set up sending data to mongodb

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
 const cors = require('@fastify/cors');
-let fastify = require('fastify');
+const fastify = require('fastify');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 let fastifyOptions = { logger: true };
 
@@ -11,29 +12,58 @@ if (process.env.NODE_ENV === 'production') {
   console.log('Production mode');
   fastifyOptions.https = {
     key: fs.readFileSync(path.join(__dirname, '.privkey-norenko.net.ua.pem')),
-    cert: fs.readFileSync(path.join(__dirname, '.fullchain-norenko.net.ua.pem')),
+    cert: fs.readFileSync(path.join(__dirname, '.fullchain-norenko.net.ua.pem'))
   };
 
   fastifyOptions.http2 = true;
 }
 
-fastify = fastify(fastifyOptions);
+const server = fastify(fastifyOptions);
 
-fastify.register(cors, {
+server.register(cors, {
   origin: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 });
 
-fastify.get('/api/questions', async (request, reply) => {
-  // send json data from file to client
-  reply.code(200).header('Content-Type', 'application/json; charset=utf-8').send(require('@/app/test/questions/questions.json'));
+// connect to mongodb
+const credentials = '.X509-cert-7088078844500846365.pem';
+
+const client = new MongoClient(`${process.env.MONGODB_URI}`, {
+  sslKey: credentials,
+  sslCert: credentials,
+  serverApi: ServerApiVersion.v1
 });
 
-fastify.listen({ port: 4000, host: '0.0.0.0' }, function (err, address) {
-  if (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-  fastify.log.info(`server listening on ${address}`);
+client.connect().then((client) => {
+  // store the db connection in the fastify instance
+  server.decorate('mongo', client.db('rhp-course'));
+
+  // Create the route
+  server.post('/api/payment', async (request, reply) => {
+    const { email, phone, name, tariff, price } = request.body;
+    console.log(request.body);
+    try {
+      const collection = server.mongo.collection('form-data');
+      const result = await collection.insertOne({ email, phone, name, tariff, price });
+      reply.code(200).send({ _id: result.insertedId });
+    } catch (err) {
+      server.log.error(err);
+      reply.code(500).send({ error: 'Failed to insert data' });
+    }
+  });
+
+  // start the server
+  server.listen({ port: 4000, host: '0.0.0.0' }, function(err, address) {
+    if (err) {
+      server.log.error(err);
+      process.exit(1);
+    }
+
+    server.log.info(`server listening on ${address}`);
+  });
+
+}).catch((err) => {
+  server.log.error('Failed to connect to MongoDB', err);
+  process.exit(1);
 });
