70886c96428435a2089ca0b5b165b778920bf996
Nikita <norenkonikita@gmail.com>
2023-06-09 16:03:20 +0300

    payments testing

--- app/kursrhp/page.js
+++ app/kursrhp/page.js
@@ -1,4 +1,4 @@
-import "@/app/kursrhp/styles.scss";
+import '@/app/kursrhp/styles.scss';
 import Main from '@/app/kursrhp/components/main/main';
 import CourseFor from '@/app/kursrhp/components/course-for/course-for';
 import CourseFormat from '@/app/kursrhp/components/course-format/course-format';
@@ -7,8 +7,8 @@ import Reviews from '@/app/kursrhp/components/reviews/reviews';
 import CourseStructure from '@/app/kursrhp/components/course-structure/course-structure';
 import Prices from '@/app/kursrhp/components/prices/prices';
 import Faq from '@/app/kursrhp/components/faq/faq';
-import Support from "./components/support/support";
-import Footer from "./components/footer/footer";
+import Support from './components/support/support';
+import Footer from './components/footer/footer';
 
 export default function KursRHPPage() {
   return (
@@ -23,6 +23,8 @@ export default function KursRHPPage() {
       <Faq />
       <Support />
       <Footer />
+
+
     </>
   );
 }
