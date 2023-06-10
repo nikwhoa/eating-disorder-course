d6f7e7ebf8b3cf4f3813c79d35ebe10767aee346
Nikita <norenkonikita@gmail.com>
2023-06-10 19:47:59 +0300

    working on desktop design. finish everything before structure of the course

--- app/kursrhp/components/reviews/reviews.js
+++ app/kursrhp/components/reviews/reviews.js
@@ -9,16 +9,67 @@ import slide4 from '@/app/kursrhp/components/reviews/images/review-four.jpg';
 import slide5 from '@/app/kursrhp/components/reviews/images/review-five.jpg';
 import slide6 from '@/app/kursrhp/components/reviews/images/review-six.jpg';
 import slide7 from '@/app/kursrhp/components/reviews/images/review-seven.jpg';
+import { useEffect } from 'react';
+import { SliderNextBtn, SliderPrevBtn } from '@/app/tools/icons/icons';
 
 export default function Reviews() {
   register();
+
+  useEffect(() => {
+    // swiper element
+    const swiperEl = document.querySelector('swiper-container');
+    const nextBtn = document.querySelector('.next-slide-button');
+    const prevBtn = document.querySelector('.prev-slide-button');
+    // swiper parameters
+    const swiperParams = {
+      slidesPerView: 1,
+      breakpoints: {
+        768: {
+          slidesPerView: 2
+        }
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        bulletClass: 'swiper-pagination-bullet',
+        clickable: true,
+        bulletActiveClass: 'swiper-pagination-bullet-active'
+      },
+      on: {
+        init() {
+          // custom next button
+          nextBtn.addEventListener('click', () => {
+            this.slideNext();
+          });
+          prevBtn.addEventListener('click', () => {
+            this.slidePrev();
+          });
+          const bullets = document.querySelectorAll('.swiper-pagination-bullet');
+          bullets.forEach((bullet, index) => {
+              bullet.style.background = '#DBC3DB';
+            }
+          );
+        }
+      }
+    };
+
+    // now we need to assign all parameters to Swiper element
+    Object.assign(swiperEl, swiperParams);
+
+    // and now initialize it
+    swiperEl.initialize();
+
+  }, []);
+
   return (
-    <section className='reviews'>
-      <div className='reviews__title'>
+    <section className='reviews container'>
+      <div className='reviews__title title-primary'>
         Відгуки
       </div>
-      <div className='reviews__slider swiper-container'>
-        <swiper-container navigation={true} pagination={true} loop={true}>
+      <div className='reviews__slider swiper-container' style={{
+        position: 'relative'
+      }}>
+        <swiper-container pagination={true} loop={true} init={false}>
           <swiper-slide key={'slide1'}>
             <div className='reviews__slide'>
               <Image src={slide1} alt='Відгук 1' />
@@ -55,7 +106,16 @@ export default function Reviews() {
             </div>
           </swiper-slide>
         </swiper-container>
+        <div className='swiper-pagination'>
+          <div className='swiper-pagination-bullet'></div>
+        </div>
+        <div className='reviews nav-buttons prev-slide-button'>
+          <SliderPrevBtn />
+        </div>
+        <div className='reviews nav-buttons next-slide-button'>
+          <SliderNextBtn />
+        </div>
       </div>
     </section>
-);
+  );
 }
