25b520c3219c9294dde389a90a59f84065823eaf
Nikita <norenkonikita@gmail.com>
2023-06-13 17:44:17 +0300

    set up sending emails after success payment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const fs = require('fs');
+const nodemailer = require('nodemailer');
 const path = require('path');
 const cors = require('@fastify/cors');
 const fastify = require('fastify');
@@ -64,6 +65,39 @@ client.connect().then((client) => {
       server.log.error(err);
       reply.code(500).send({ error: 'Failed to insert data' });
     }
+  });
+
+
+  /*
+  * Sending emails
+  * */
+  server.post('/api/send-email', async (req, res) => {
+
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'norenkonikita@gmail.com',
+        pass: 'juxaryggtouvajhk'
+      }
+    });
+
+    let mailOptions = {
+      from: 'norenkonikita@gmail.com',
+      to: req.body.to,
+      subject: req.body.subject,
+      text: req.body.text,
+      html: req.body.html
+    };
+
+    transporter.sendMail(mailOptions, function(error, info) {
+      if (error) {
+        console.log(error);
+        res.status(500).send(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+        res.status(200).send('Email sent: ' + info.response);
+      }
+    });
   });
 
   // start the server
