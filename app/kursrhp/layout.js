aef0795a44775cc378fec6041839f6eb170c1ad4
Nikita <norenkonikita@gmail.com>
2023-06-18 23:05:21 +0300

    PRAVKIIIIII!

--- app/kursrhp/layout.js
+++ app/kursrhp/layout.js
@@ -1,4 +1,10 @@
 
+export const metadata = {
+  title: 'Курс: Relife',
+  description: 'Relife - вилікуватись від компульсивних переїдань, булімії, орторексії раз і назавжди',
+  favicon: './favicons/favicon.ico'
+};
+
 export default function RootLayout({ children }) {
   return (
     <main>
--- app/test/layout.js
+++ app/test/layout.js
@@ -1,5 +1,11 @@
 import '@/app/test/styles/globals.scss';
 
+export const metadata = {
+  title: 'Ставлення до прийому їжі (EAT-26)',
+  description: 'Тест ставлення до прийому їжі (Eating Attitudes Test (EAT-26))',
+  favicon: './favicons/favicon.ico'
+};
+
 export default function RootLayout({ children }) {
   return (
     <div className=''>
