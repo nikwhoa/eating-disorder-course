718ed60f7511300c6fbd90ca0019b424ec4d6e89
Nikita <norenkonikita@gmail.com>
2023-06-18 23:05:21 +0300

    PRAVKIIIIII!

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,6 @@
 // import '@/app/questions/styles/globals.scss';
 
-export const metadata = {
-  title: 'Ставлення до прийому їжі (EAT-26)',
-  description: 'Тест ставлення до прийому їжі (Eating Attitudes Test (EAT-26))',
-  favicon: './favicons/favicon.ico'
-};
+
 
 export default function RootLayout({ children }) {
   return (
