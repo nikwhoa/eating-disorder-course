a80a6c46b9b25c820f9c6b1085a445fb7dc7b0c2
Nikita <norenkonikita@gmail.com>
2023-06-23 16:53:01 +0300

    fix email delivery set up

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 require('dotenv').config();
 const fs = require('fs');
-const sendpulse = require('sendpulse-api');
-const nodemailer = require('nodemailer');
 const path = require('path');
 const cors = require('@fastify/cors');
 const fastify = require('fastify');
-const { MongoClient, ServerApiVersion } = require('mongodb');
 
 let fastifyOptions = { logger: true };
 
@@ -28,50 +25,59 @@ server.register(cors, {
   allowedHeaders: ['Content-Type', 'Authorization']
 });
 
-// connect to mongodb
-const credentials = '.X509-cert-7088078844500846365.pem';
-
-const client = new MongoClient(`${process.env.MONGODB_URI}`, {
-  sslKey: credentials,
-  sslCert: credentials,
-  serverApi: ServerApiVersion.v1
+server.get('/', async (req, res) => {
+  res.send({ message: 'Hello world' });
 });
 
-// console.log(client);
+server.post('/api/payment', async (request, reply) => {
+  reply.send({ message: 'Payment is processing...' });
+});
 
-client.connect().then((client) => {
+server.post('/api/send-email', async (req, res) => {
 
-  /*
-  * Sending emails
-  * */
-  server.post('/api/send-email', async (req, res) => {
+  const API_USER_ID = process.env.NEXT_PUBLIC_SENDPULSE_USER_ID;
+  const API_SECRET = process.env.NEXT_PUBLIC_SENDPULSE_SECRET;
 
-    const API_USER_ID = process.env.NEXT_PUBLIC_SENDPULSE_USER_ID;
-    const API_SECRET = process.env.NEXT_PUBLIC_SENDPULSE_SECRET;
-    const TOKEN_STORAGE = fs.readdirSync('/tmp/');
-
-    sendpulse.init(API_USER_ID, API_SECRET, TOKEN_STORAGE, (token) => {
-      if (token && token.is_error) {
-        throw new Error('Error: ', token.errors);
+  const token = await new Promise((resolve, reject) => {
+    fetch('https://api.sendpulse.com/oauth/access_token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          grant_type: 'client_credentials',
+          client_id: API_USER_ID,
+          client_secret: API_SECRET
+        })
       }
+    ).then(res => res.json()).then((data) => {
+      resolve(data.access_token);
+    }).catch((error) => {
+      reject(error);
+    });
+  });
 
-      /**
-       * Function to process response data
-       *
-       * @param data
-       */
-      const answerGetter = function(data) {
-        console.log(data);
-      };
-
-      sendpulse.addEmails(answerGetter, 238442, [{
-        email: req.body.to, variables: {
-          'Ім\'я': req.body.name,
-          tariff: req.body.tariff,
-          Phone: req.body.phone
+  fetch('https://api.sendpulse.com/addressbooks/238442/emails', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    },
+    body: JSON.stringify({
+      emails: [
+        {
+          email: req.body.to,
+          variables: {
+            'Імʼя': req.body.name,
+            phone: req.body.phone,
+            tariff: req.body.tariff
+          }
         }
-      }]);
-
+      ]
+    })
+  }).then((response) => {
+    if (response.status === 200) {
+      // res.status(200).send({ message: 'Email sent successfully' });
       fetch('https://events.sendpulse.com/events/name/purchase_2', {
         method: 'POST',
         headers: {
@@ -87,72 +93,16 @@ client.connect().then((client) => {
           res.status(200).send({ message: 'Email sent successfully' });
         }
       });
-    });
-
-
-  });
-
-  // console.log(client);
-  console.log('Connected successfully to mongodb server');
-  // store the db connection in the fastify instance
-  server.decorate('mongo', client.db('rhp-course'));
-
-  // get info from the db collection form-data
-
-  // server.get('/api/form-data', async (request, reply) => {
-  //   try {
-  //     const collection = server.mongo.collection('form-data');
-  //     const result = await collection.find().toArray();
-  //     reply.code(200).send(result);
-  //   } catch (err) {
-  //     server.log.error(err);
-  //     reply.code(500).send({ error: 'Failed to get data' });
-  //   }
-  // });
-
-  // Create the route
-  server.post('/api/payment', async (request, reply) => {
-    console.log('request.body');
-    console.log(request.body);
-    const { email, phone, name, tariff, price } = request.body;
-    console.log(request.body);
-    try {
-      const collection = server.mongo.collection('form-data');
-      const date = new Date();
-
-      // const options = { year: 'numeric', month: 'long', day: 'numeric' };
-
-      // const ukrainianDate = date.toLocaleDateString('uk-UA', options);
-      const ukrainianDate = date.toLocaleDateString('uk-UA', {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric'
-      });
-
-      const result = await collection.insertOne({ email, phone, name, tariff, price, created_at: ukrainianDate });
-
-      // const result = await collection.insertOne({ email, phone, name, tariff, price });
-      reply.code(200).send({ _id: result.insertedId });
-    } catch (err) {
-      server.log.error(err);
-      reply.code(500).send({ error: 'Failed to insert data' });
     }
   });
 
+});
 
+server.listen({ port: 4000, host: '0.0.0.0' }, function(err, address) {
+  if (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
 
-
-  // start the server
-  server.listen({ port: 4000, host: '0.0.0.0' }, function(err, address) {
-    if (err) {
-      server.log.error(err);
-      process.exit(1);
-    }
-
-    server.log.info(`server listening on ${address}`);
-  });
-
-}).catch((err) => {
-  server.log.error('Failed to connect to MongoDB', err);
-  process.exit(1);
+  server.log.info(`server listening on ${address}`);
 });
