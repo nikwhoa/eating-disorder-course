bfad4dd06ed9ed17d5acfe8329ad61daf3091dfa
Nikita <norenkonikita@gmail.com>
2023-06-23 18:43:12 +0300

    trying to send email. lol

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-require('dotenv').config();
-const fs = require('fs');
-const path = require('path');
-const cors = require('@fastify/cors');
-const fastify = require('fastify');
+import 'dotenv/config'
+import fetch from 'node-fetch';
+import fs from 'fs';
+// require('dotenv').config();
+// const fs = require('fs');
+import path from 'path';
+import cors from '@fastify/cors';
+import fastify from 'fastify';
+// const path = require('path');
+// const cors = require('@fastify/cors');
+// const fastify = require('fastify');
 
 let fastifyOptions = { logger: true };
 
@@ -11,7 +17,7 @@ if (process.env.NODE_ENV === 'production') {
   console.log('Production mode');
   fastifyOptions.https = {
     key: fs.readFileSync(path.join(__dirname, '.privkey-norenko.net.ua.pem')),
-    cert: fs.readFileSync(path.join(__dirname, '.fullchain-norenko.net.ua.pem'))
+    cert: fs.readFileSync(path.join(__dirname, '.fullchain-norenko.net.ua.pem')),
   };
 
   fastifyOptions.http2 = true;
@@ -21,7 +27,7 @@ const server = fastify(fastifyOptions);
 server.register(cors, {
   origin: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization'],
 });
 
 server.get('/', async (req, res) => {
@@ -33,7 +39,6 @@ server.post('/api/payment', async (request, reply) => {
 });
 
 server.post('/api/send-email', async (req, res) => {
-
   const API_USER_ID = process.env.NEXT_PUBLIC_SENDPULSE_USER_ID;
   const API_SECRET = process.env.NEXT_PUBLIC_SENDPULSE_SECRET;
 
@@ -42,13 +47,13 @@ server.post('/api/send-email', async (req, res) => {
       const response = await fetch('https://api.sendpulse.com/oauth/access_token', {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({
           grant_type: 'client_credentials',
           client_id: API_USER_ID,
-          client_secret: API_SECRET
-        })
+          client_secret: API_SECRET,
+        }),
       });
       const data = await response.json();
       return data.access_token;
@@ -63,20 +68,20 @@ server.post('/api/send-email', async (req, res) => {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`
+          Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify({
           emails: [
             {
               email: req.body.to,
               variables: {
-                'Імʼя': req.body.name,
+                Імʼя: req.body.name,
                 phone: req.body.phone,
-                tariff: req.body.tariff
-              }
-            }
-          ]
-        })
+                tariff: req.body.tariff,
+              },
+            },
+          ],
+        }),
       });
       if (response.status === 200) {
         server.log.info('Email added successfully');
@@ -84,12 +89,12 @@ server.post('/api/send-email', async (req, res) => {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
+            Authorization: `Bearer ${token}`,
           },
           body: JSON.stringify({
-            'email': req.body.to,
-            'tariff': req.body.tariff
-          })
+            email: req.body.to,
+            tariff: req.body.tariff,
+          }),
         });
         if (eventResponse.status === 200) {
           server.log.info('Email sent successfully');
@@ -107,11 +112,9 @@ server.post('/api/send-email', async (req, res) => {
   } catch (error) {
     server.log.error(error);
   }
-
-
 });
 
-server.listen({ port: 4000, host: '0.0.0.0' }, function(err, address) {
+server.listen({ port: 4000, host: '0.0.0.0' }, function (err, address) {
   if (err) {
     server.log.error(err);
     process.exit(1);
