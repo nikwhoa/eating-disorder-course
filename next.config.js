1d7de8d66890186fc84c9fd8d59a392dc25e5baf
Nikita <norenkonikita@gmail.com>
2024-01-07 01:52:47 +0200

    commit

--- next.config.js
+++ next.config.js
@@ -1,20 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: false,
+  output: "export",
+  basePath: '',
+  // assetPrefix: "./",
+  trailingSlash: true,
+  images: { unoptimized: true },
   env: {
     DB_HOST: process.env.DB_HOST
   },
   compiler: {
     styledComponents: true
   },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/test',
-        permanent: false
-      }
-    ];
-  }
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/',
+  //       destination: '/test',
+  //       permanent: false
+  //     }
+  //   ];
+  // }
 };
 export default nextConfig;
 // module.exports = nextConfig;
